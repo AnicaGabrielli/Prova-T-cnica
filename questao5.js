1383ae7876b548a71c38ecb91cc63f7573afae3e
AnicaGabrielli <113793660+AnicaGabrielli@users.noreply.github.com>
2025-01-10 12:53:18 -0300

    Create questao5.js

---
+++ questao5.js
@@ -0,0 +1,9 @@
+function inverterString(texto) {
+    let invertida = "";
+    for (let i = texto.length - 1; i >= 0; i--) {
+        invertida += texto[i];
+    }
+    return invertida;
+}
+const string = "Anica";
+console.log(`String invertida: ${inverterString(string)}`);
